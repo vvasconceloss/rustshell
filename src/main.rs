5c33b974e782ec161fb5171dee7e918e91c9b844
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-22 13:08:20 +0000

    feat: implement an error command for a 'command not found'

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,6 @@ fn main() {
     let stdin = io::stdin();
     let mut user_input = String::new();
     stdin.read_line(&mut user_input).unwrap();
+
+    println!("{}: command not found", user_input.trim());
 }
