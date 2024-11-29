269f3f0e83f91a43f2f58d08d315acb5777734ab
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-11-29 14:46:06 +0000

    feat: implement the function that searches the user's current directory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::env;
+use std::{io::{self, Write}, path::PathBuf};
+
+fn get_current_directory() -> std::io::Result<PathBuf> {
+     env::current_dir()
+}
 
 fn main() {
     loop {
-        print!("$ ");
+        let working_dir = get_current_directory().unwrap();
+
+        print!("~ {}>", working_dir.display());
         io::stdout().flush().unwrap();
 
         let stdin = io::stdin();
