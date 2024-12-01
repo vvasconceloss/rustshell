7c4bf261a971b7745eb530a6e517b743d3db0c7a
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2024-12-01 11:32:35 +0000

    feat: create implement command parsing
    - create an enum to store internal system commands (cd and exit)
    - create a parse_commands function to parse user commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@
 use std::env;
 use std::{io::{self, Write}, path::PathBuf};
 
+enum InternalCommand {
+    Exit,
+    Cd(String),
+}
+
+impl InternalCommand {
+    fn parse_command(input: &str) -> Option<Self> {
+        let args: Vec<&str> = input.split_whitespace().collect();
+        match args.get(0) {
+            Some(&"cd") => Some(InternalCommand::Cd(args.get(1).cloned().unwrap_or_default().to_string())),
+            Some(&"exit") => Some(InternalCommand::Exit),
+            _ => None,
+        }
+    }
+}
+
 fn get_current_directory() -> std::io::Result<PathBuf> {
      env::current_dir()
 }
@@ -10,18 +26,13 @@ fn main() {
     loop {
         let working_dir = get_current_directory().unwrap();
 
-        print!("~ {}>", working_dir.display());
+        print!("~ {}> ", working_dir.display());
         io::stdout().flush().unwrap();
 
         let stdin = io::stdin();
         let mut user_input = String::new();
         stdin.read_line(&mut user_input).unwrap();
 
-        match user_input.trim().to_lowercase().as_str() {
-            "" => continue,
-            "exit" => break,
-            "exit 0" => break,
-            _ => println!("{}: command not found", user_input.trim()),
-        }   
+        let input_user = user_input.trim().to_lowercase().to_string();
     }
 }
